73bcdc08220d86cd2e113a26d1d55edd329b1a30
r-bytes <101632683+r-bytes@users.noreply.github.com>
2022-10-04 18:42:47 +0200

    Moved Styled Components to seperate file

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Chat from "./components/Chat";
 import Sidebar from "./components/Sidebar";
-import styled from "styled-components";
+import { Wrapper, AppContainer } from "./styles/AppStyles"
+
 
 function App() {
     return (
@@ -14,18 +15,3 @@ function App() {
 }
 
 export default App;
-
-const Wrapper = styled.div`
-    display: grid;
-    place-items: center;
-    height: 100vh;
-    background-color: #dadbd3;
-`
-const AppContainer = styled.div`
-    display: flex;
-    background-color: #ededed;
-    margin-top: -50px;
-    height: 90vh;
-    width: 90vw;
-    box-shadow: -1px 4px 20px -6px rgba(0, 0, 0, .75);
-`
