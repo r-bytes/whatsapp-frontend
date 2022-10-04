2de98f5736e678cc9740af4f2deafef7fcd956e3
r-bytes <101632683+r-bytes@users.noreply.github.com>
2022-10-04 15:22:40 +0200

    added global styles

---
+++ src/styles/theme.js
@@ -0,0 +1,11 @@
+import { ThemeProvider } from 'styled-components';
+
+import theme from "../themes/default";
+
+const Theme = ({children}) => (
+    <ThemeProvider theme={theme}>
+        {children}
+    </ThemeProvider>
+);
+
+export default Theme;
---
+++ src/themes/default.js
@@ -0,0 +1,10 @@
+export default {
+    // Breakpoints for responsive design
+      breakpoints: {
+        xs: 'screen and (min-width: 480px)',
+        sm: 'screen and (min-width: 640px)',
+        md: 'screen and (min-width: 768px)',
+        lg: 'screen and (min-width: 1024px)',
+        xl: 'screen and (min-width: 1280px)'
+      }
+}
