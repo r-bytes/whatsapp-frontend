389f72e0c41438412c709b8f908e19235dfac5b8
r-bytes <101632683+r-bytes@users.noreply.github.com>
2022-10-05 17:29:08 +0200

    added useEffects to fetch data from the backend

--- src/App.js
+++ src/App.js
@@ -1,27 +1,47 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import Chat from "./components/Chat";
 import Sidebar from "./components/Sidebar";
 import { Wrapper, AppContainer } from "./styles/AppStyles"
 import Pusher from "pusher-js"
+import axios from "./axios.js"
 
 
 function App() {
+    const [messages, setMessages] = useState([])
+    
+    useEffect(() => {
+        axios.get("/api/v1/messages/sync")
+            .then( response => {
+                setMessages(response.data)
+        })
 
+    }, []);
+
+    
     useEffect(() => {
         const pusher = new Pusher('2dc9c34e35fd049b26e7', {
             cluster: 'eu'
-          });
-      
-          const channel = pusher.subscribe('messages');
-          channel.bind('inserted', (data) => {
-            alert(JSON.stringify(data));
-          });
-    }, []);
+        });
+        
+        const channel = pusher.subscribe('messages');
+        channel.bind('inserted', (newMessage) => {
+            setMessages([...messages, newMessage])
+        });
+        
+        return () => {
+            channel.unbind()
+            channel.unsubscribe()
+        }
+    }, [messages]);
+    
+    console.log(messages)
+    
+    
     return (
         <Wrapper>
             <AppContainer>
                 <Sidebar />
-                <Chat />
+                <Chat messages={messages} />
             </AppContainer>
         </Wrapper>
     );
