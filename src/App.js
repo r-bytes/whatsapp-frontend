0caa94c0941f2667a01ec503e15f3ca0b53f445c
r-bytes <101632683+r-bytes@users.noreply.github.com>
2022-10-05 14:32:19 +0200

    Connected Pusher with the frontend

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
+import { useEffect } from "react";
 import Chat from "./components/Chat";
 import Sidebar from "./components/Sidebar";
 import { Wrapper, AppContainer } from "./styles/AppStyles"
+import Pusher from "pusher-js"
 
 
 function App() {
+
+    useEffect(() => {
+        const pusher = new Pusher('2dc9c34e35fd049b26e7', {
+            cluster: 'eu'
+          });
+      
+          const channel = pusher.subscribe('messages');
+          channel.bind('inserted', (data) => {
+            alert(JSON.stringify(data));
+          });
+    }, []);
     return (
         <Wrapper>
             <AppContainer>
