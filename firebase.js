3b9f62f80585dd1c212100b060fa3c2f51c34536
r-bytes <101632683+r-bytes@users.noreply.github.com>
2022-10-07 11:22:13 +0200

    added firebase.js

---
+++ firebase.js
@@ -0,0 +1,21 @@
+import firebase from "firebase";
+import * as dotenv from 'dotenv'
+
+dotenv.config()
+
+const firebaseConfig = {
+    apiKey: process.env.REACT_APP_FIREBASE_apiKey,
+    authDomain: process.env.REACT_APP_FIREBASE_authDomain,
+    projectId: process.env.REACT_APP_FIREBASE_projectId,
+    storageBucket: process.env.REACT_APP_FIREBASE_storageBucket,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_messagingSenderId,
+    appId: process.env.REACT_APP_FIREBASE_appId
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig)
+const db = firebaseApp.firestore()
+const auth = firebase.auth()
+const provider = new firebase.auth.GoogleAuthProvider()
+
+export { auth, provider }
+export default db
