2240759c5956608fdba8a40553a2d5b2b81409e9
r-bytes <101632683+r-bytes@users.noreply.github.com>
2022-10-17 17:07:06 +0200

    refactor mongodb to firestore col inside col N/A

--- src/components/SidebarChat.jsx
+++ src/components/SidebarChat.jsx
@@ -1,34 +1,48 @@
 import { Avatar } from "@material-ui/core"
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { SidebarChatContainer, SidebarChatInfo } from "../styles/SidebarChatStyles"
 import { usePusherContext } from "../context/PusherProvider"
-import axios from "../axios.js"
 import { Link } from "react-router-dom"
+import { db } from "../firebase"
+import { collection, doc, getDocs, onSnapshot, setDoc } from "firebase/firestore";
+import { nanoid } from "nanoid"
+
 
 const SidebarChat = ({addNewChat}) => {
-    const { groups, setGroups } = usePusherContext()
+    const [groups, setGroups] = useState([]);
 
-    const createChat = () => {
+    useEffect(() => {
+        const unsubscribe = onSnapshot(collection(db, "groups"), snapshot => {
+            setGroups(snapshot.docs.map(doc => ({
+                id: doc.id,
+                data: doc.data()
+            })))
+        })
+
+        return () => {
+            unsubscribe()
+        }
+    }, []);
+
+    const createChat = async () => {
         const groupName = prompt("Please enter a name for the chat")
 
+        // add group to firestore db
         if (groupName) {
-            axios.post("/api/v1/group/new", {
-                name: groupName
-            })
-              .then( response => {
-                setGroups([...groups, {_id: response.data._id, name: response.data.name}])
+            await setDoc(doc(db, "groups", nanoid()), {
+                name: groupName,
             })
         }
     }
 
     return !addNewChat ? (
         groups.map(group => (
-            <Link to={`/groups/${group._id}`} key={group._id}>
+            <Link to={`/groups/${group.id}`} key={group.id}>
                 <SidebarChatContainer>
                     <Avatar src={`https://avatars.dicebear.com/api/human/${group.name}.svg`} />
                     <SidebarChatInfo>
-                        <h2> {group.name} </h2>
-                        <p> This is the last message </p>
+                        <h2> {group.data.name} </h2>
+                        <p> last message... </p>
                     </SidebarChatInfo>
                 </SidebarChatContainer>
             </Link>
